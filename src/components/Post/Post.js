3beb3196237e4836e86c923fb6a4715f0332792a
NachoKai <nachokai@outlook.com>
2020-04-28 01:26:06 -0300

    test

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Post.scss";
 
 const Post = () => {
-  return <div className="post">Post</div>;
+  return <div className="post">Post.</div>;
 };
 
 export default Post;
