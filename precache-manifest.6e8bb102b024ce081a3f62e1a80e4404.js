0b0514ffdf9ca030bbdabb97ae6a5151793a6e19
NachoKai <nachokai@outlook.com>
2020-04-28 01:23:23 -0300

    Updates

---
+++ precache-manifest.6e8bb102b024ce081a3f62e1a80e4404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6385447bb5ce33052de131309ab1e4",
+    "url": "/cara-de-libro/index.html"
+  },
+  {
+    "revision": "9e733a37958fc68a1865",
+    "url": "/cara-de-libro/static/css/main.8fa2dd26.chunk.css"
+  },
+  {
+    "revision": "42f3e32f9eab36264d92",
+    "url": "/cara-de-libro/static/js/2.92546b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cara-de-libro/static/js/2.92546b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e733a37958fc68a1865",
+    "url": "/cara-de-libro/static/js/main.e5a9749c.chunk.js"
+  },
+  {
+    "revision": "6e9c2c18045b8b5c6117",
+    "url": "/cara-de-libro/static/js/runtime-main.7c54fc28.js"
+  }
+]);
