47368a5a58233538f3c79696b077a6261ec5ba73
NachoKai <nachokai@outlook.com>
2020-04-29 01:05:39 -0300

    Updates

---
+++ precache-manifest.d66342ebd8eb80908cabc3d8ca384ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da30da56ee0eebbe0803c3d39af2478",
+    "url": "/cara-de-libro/index.html"
+  },
+  {
+    "revision": "353c9ed1108a4141ab97",
+    "url": "/cara-de-libro/static/css/main.07e2208d.chunk.css"
+  },
+  {
+    "revision": "7f938c3a1199a4b40f99",
+    "url": "/cara-de-libro/static/js/2.05c6a592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cara-de-libro/static/js/2.05c6a592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353c9ed1108a4141ab97",
+    "url": "/cara-de-libro/static/js/main.6df5e242.chunk.js"
+  },
+  {
+    "revision": "6e9c2c18045b8b5c6117",
+    "url": "/cara-de-libro/static/js/runtime-main.7c54fc28.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cara-de-libro/precache-manifest.c31f4ce8b88d73f121f5425a5e223d97.js"
+  "/cara-de-libro/precache-manifest.d66342ebd8eb80908cabc3d8ca384ae2.js"
 );
 
 self.addEventListener('message', (event) => {
